734a8ce1267f401efdebaae98eb290d4af7671db
Zi Ye <18612319597@163.com>
2020-07-08 22:24:33 +0800

    feat: transform request data

--- src/helpers/url.ts
+++ src/helpers/url.ts
@@ -1,54 +1,54 @@
-import { isDate, isObject } from './util'
+import { isDate, isPlainObject } from './util'
 
 function encode(val: string): string {
   return encodeURIComponent(val)
     .replace(/%40/g, '@')
-    .replace(/%3a/ig, ':')
+    .replace(/%3a/gi, ':')
     .replace(/%24/g, '$')
-    .replace(/%2c/ig, ',')
+    .replace(/%2c/gi, ',')
     .replace(/%20/g, '+')
-    .replace(/%5b/ig, '[')
-    .replace(/%5d/ig, ']');
+    .replace(/%5b/gi, '[')
+    .replace(/%5d/gi, ']')
 }
 
 export function buildURL(url: string, params?: any): string {
-  if(!params) {
-    return url;
+  if (!params) {
+    return url
   }
 
-  const parts: string[] = [];
+  const parts: string[] = []
 
   Object.keys(params).forEach(key => {
-    const val = params[key];
-    if(val === null || typeof val === 'undefined') {
-      return;
+    const val = params[key]
+    if (val === null || typeof val === 'undefined') {
+      return
     }
-    let values = [];
-    if(Array.isArray(val)) {
-      values = val;
-      key =  `${key}[]`;
+    let values = []
+    if (Array.isArray(val)) {
+      values = val
+      key = `${key}[]`
     } else {
-      values = [val];
+      values = [val]
     }
     values.forEach(item => {
-      if(isDate(item)) {
-        item = item.toISOString();
-      } else if(isObject(item)) {
-        item = JSON.stringify(item);
+      if (isDate(item)) {
+        item = item.toISOString()
+      } else if (isPlainObject(item)) {
+        item = JSON.stringify(item)
       }
-      parts.push(`${encode(key)}=${encode(item)}`);
+      parts.push(`${encode(key)}=${encode(item)}`)
     })
   })
 
-  const serializedParams = parts.join('&');
+  const serializedParams = parts.join('&')
 
-  if(serializedParams) {
-    const markIndex = url.indexOf('#');
-    if(markIndex !== -1) {
-      url = url.slice(0, markIndex);
+  if (serializedParams) {
+    const markIndex = url.indexOf('#')
+    if (markIndex !== -1) {
+      url = url.slice(0, markIndex)
     }
-    url += `${url.indexOf('?') === -1 ? '?' : '&'}${serializedParams}`;
+    url += `${url.indexOf('?') === -1 ? '?' : '&'}${serializedParams}`
   }
 
-  return url;
+  return url
 }
