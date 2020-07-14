8209edf5d0990eb07a8232e0fa73e35c897c1d5a
Zi Ye <18612319597@163.com>
2020-07-14 22:30:23 +0800

    feat(extend/app.ts examples/server.js): extend demo

---
+++ examples/extend/app.ts
@@ -0,0 +1,26 @@
+import axios from '../../src';
+
+axios({
+  url: '/extend/post',
+  method: 'post',
+  data: {
+    msg: 'Hello'
+  }
+});
+
+axios.request({
+  url: '/extend/post',
+  method: 'post',
+  data: {
+    msg: 'Hello'
+  }
+});
+
+axios.get('/extend/get');
+axios.delete('/extend/delete');
+axios.head('/extend/head');
+axios.options('/extend/options');
+
+axios.post('/extend/post', { msg: 'post' });
+axios.put('/extend/put', { msg: 'put' });
+axios.patch('/extend/patch', { msg: 'patch' });
--- examples/server.js
+++ examples/server.js
@@ -27,51 +27,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const router = express.Router();
 
-router.get('/simple/get', function(req, res) {
-  res.json({
-    msg: 'Hello World'
-  });
-});
+registerSimpleRouter();
 
-router.get('/base/get', function(req, res) {
-  res.json(req.query);
-});
+registerBaseRouter();
 
-router.post('/base/post', function(req, res) {
-  res.json(req.body);
-});
+registerErrorRouter();
 
-router.post('/base/buffer', function(req, res) {
-  const msg = [];
-  req.on('data', chunk => {
-    if (chunk) {
-      msg.push(chunk);
-    }
-  });
-  req.on('end', () => {
-    const buf = Buffer.concat(msg);
-    res.json(buf.toJSON());
-  });
-});
-
-router.get('/error/get', function(req, res) {
-  if (Math.random() > 0.5) {
-    res.json({
-      msg: 'Hello World'
-    });
-  } else {
-    res.status(500);
-    res.end();
-  }
-});
-
-router.get('/error/timeout', function(req, res) {
-  setTimeout(() => {
-    res.json({
-      msg: 'Hello World'
-    });
-  }, 3000);
-});
+registerExtendRouter();
 
 app.use(router);
 
@@ -80,3 +42,87 @@ const port = process.env.PORT || 8080;
 module.exports = app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}, Control+C to stop`);
 });
+
+function registerSimpleRouter() {
+  router.get('/simple/get', function(req, res) {
+    res.json({
+      msg: 'Hello World'
+    });
+  });
+}
+
+function registerBaseRouter() {
+  router.get('/base/get', function(req, res) {
+    res.json(req.query);
+  });
+
+  router.post('/base/post', function(req, res) {
+    res.json(req.body);
+  });
+
+  router.post('/base/buffer', function(req, res) {
+    const msg = [];
+    req.on('data', chunk => {
+      if (chunk) {
+        msg.push(chunk);
+      }
+    });
+    req.on('end', () => {
+      const buf = Buffer.concat(msg);
+      res.json(buf.toJSON());
+    });
+  });
+}
+
+function registerErrorRouter() {
+  router.get('/error/get', function(req, res) {
+    if (Math.random() > 0.5) {
+      res.json({
+        msg: 'Hello World'
+      });
+    } else {
+      res.status(500);
+      res.end();
+    }
+  });
+
+  router.get('/error/timeout', function(req, res) {
+    setTimeout(() => {
+      res.json({
+        msg: 'Hello World'
+      });
+    }, 3000);
+  });
+}
+
+function registerExtendRouter() {
+  router.get('/extend/get', function(req, res) {
+    res.json({
+      msg: 'Hello World'
+    });
+  });
+
+  router.delete('/extend/delete', function(req, res) {
+    res.end()
+  });
+
+  router.head('/extend/head', function(req, res) {
+    res.end()
+  });
+
+  router.options('/extend/options', function(req, res) {
+    res.end()
+  });
+
+  router.post('/extend/post', function(req, res) {
+    res.json(req.body);
+  });
+
+  router.put('/extend/put', function(req, res) {
+    res.json(req.body);
+  });
+
+  router.patch('/extend/patch', function(req, res) {
+    res.json(req.body);
+  });
+}
