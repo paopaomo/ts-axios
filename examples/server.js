e54fb44cc43f40ff204ed9dec6e768eaf91ffad4
Zi Ye <18612319597@163.com>
2020-07-15 20:12:13 +0800

    feat(example/interceptor/): interceptor demo

--- examples/server.js
+++ examples/server.js
@@ -35,6 +35,8 @@ registerErrorRouter();
 
 registerExtendRouter();
 
+registerInterceptorRouter();
+
 app.use(router);
 
 const port = process.env.PORT || 8080;
@@ -135,5 +137,11 @@ function registerExtendRouter() {
         age: 18
       }
     });
+  });
+}
+
+function registerInterceptorRouter() {
+  router.get('/interceptor/get', function(req, res) {
+    res.end('Hello World');
   });
 }
