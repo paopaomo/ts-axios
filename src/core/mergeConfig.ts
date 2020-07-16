93ba443f35c93063533e4db6fda26372639fb8da
Zi Ye <18612319597@163.com>
2020-07-16 20:10:32 +0800

    feat(mergeConfig.ts): deep merge config

--- src/core/mergeConfig.ts
+++ src/core/mergeConfig.ts
@@ -1,4 +1,5 @@
 import { AxiosRequestConfig } from '../types';
+import { deepMerge, isPlainObject } from '../helpers/util';
 
 function defaultStrategy(val1: any, val2: any): any {
   return typeof val2 === 'undefined' ? val1 : val2;
@@ -8,12 +9,33 @@ function fromVal2Strategy(val1: any, val2: any): any {
   return val2;
 }
 
+function deepMergeStrategy(val1: any, val2: any): any {
+  if (isPlainObject(val2)) {
+    return deepMerge(val1, val2);
+  }
+  if (typeof val2 !== 'undefined') {
+    return val2;
+  }
+  if (isPlainObject(val1)) {
+    return deepMerge(val1);
+  }
+  if (typeof val1 !== 'undefined') {
+    return val1;
+  }
+}
+
 const strategy = Object.create(null);
 
 const strategyKeysFromVal2 = ['url', 'params', 'data'];
 
 strategyKeysFromVal2.forEach(key => {
   strategy[key] = fromVal2Strategy;
+});
+
+const strategyKeysDeepMerge = ['headers'];
+
+strategyKeysDeepMerge.forEach(key => {
+  strategy[key] = deepMergeStrategy;
 });
 
 export default function mergeConfig(config1: AxiosRequestConfig, config2?: AxiosRequestConfig) {
