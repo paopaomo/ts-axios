475a24f00efbedd7e01661106493df647e5cea35
Zi Ye <18612319597@163.com>
2020-07-22 20:44:29 +0800

    feat(examples/more/): xsrfCookieName and xsrfHeaderName demo

--- examples/more/app.ts
+++ examples/more/app.ts
@@ -7,3 +7,10 @@ axios.get('/more/get').then(console.log);
 axios.post('http://127.0.0.1:8088/more/server2', {}, {
   withCredentials: true
 }).then(console.log);
+
+const instance = axios.create({
+  xsrfCookieName: 'XSRF-TOKEN-D',
+  xsrfHeaderName: 'X-XSRF-TOKEN-D'
+});
+
+instance.get('/more/get').then(console.log);
