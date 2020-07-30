d6b4e3e871efdf56a2bc4e71dccbc82308a89987
Zi Ye <18612319597@163.com>
2020-07-30 21:39:30 +0800

    feat: axios.all axios.spread axios.getUri

--- src/core/Axios.ts
+++ src/core/Axios.ts
@@ -6,9 +6,10 @@ import {
   RejectedFn,
   ResolvedFn
 } from '../types';
-import dispatchRequest from './dispatchRequest';
+import dispatchRequest, { transformURL } from './dispatchRequest';
 import InterceptorManager from './InterceptorManager';
 import mergeConfig from './mergeConfig';
+import defaults from '../defaults';
 
 interface Interceptors {
   request: InterceptorManager<AxiosRequestConfig>;
@@ -95,6 +96,11 @@ export default class Axios {
 
   patch(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
     return this._requestMethodWithData('patch', url, data, config);
+  }
+
+  getUri(config: AxiosRequestConfig): string {
+    config = mergeConfig(this.defaults, config);
+    return transformURL(config);
   }
 
   _requestMethodWithoutData(method: Method, url: string, config?: AxiosRequestConfig) {
