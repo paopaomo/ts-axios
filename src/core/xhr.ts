68635bf381c05bba9d49e6a57cd476e350c90db5
Zi Ye <18612319597@163.com>
2020-08-17 19:40:09 +0800

    feat(headers.spec.ts): headers test

--- src/core/xhr.ts
+++ src/core/xhr.ts
@@ -111,7 +111,7 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
       }
 
       Object.keys(headers).forEach(name => {
-        if (!data && name.toLowerCase() === 'content-type') {
+        if (data === null && name.toLowerCase() === 'content-type') {
           delete headers[name];
         } else {
           request.setRequestHeader(name, headers[name]);
