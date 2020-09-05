760da5f68d990e783dc46fb3bd17d116c728b991
Zi Ye <zi.ye1@thoughtworks.com>
2020-09-05 11:53:10 +0800

    feat(request.spec.ts): request responseType test

--- test/requests.spec.ts
+++ test/requests.spec.ts
@@ -249,4 +249,36 @@ describe('requests', () => {
       expect(request.requestHeaders['Content-Type']).toBe('application/json');
     });
   });
+
+  test('should support array buffer response', done => {
+    let response: AxiosResponse;
+
+    const str2ab = (str: string) => {
+      const buff = new ArrayBuffer(str.length * 2);
+      const view = new Uint16Array(buff);
+      for (let i = 0; i < str.length; i++) {
+        view[i] = str.charCodeAt(i);
+      }
+      return buff;
+    };
+
+    axios('/foo', {
+      responseType: 'arraybuffer'
+    }).then(res => {
+      response = res;
+    });
+
+    getAjaxRequest().then(request => {
+      request.respondWith({
+        status: 200,
+        // @ts-ignore
+        response: str2ab('Hello World')
+      });
+
+      setTimeout(() => {
+        expect(response.data.byteLength).toBe(22);
+        done();
+      }, 100);
+    });
+  });
 });
